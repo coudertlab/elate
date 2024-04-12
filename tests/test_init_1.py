58cf0e369a34aa5bce1820275a1f499b2782effd
Francois-Xavier Coudert <fxcoudert@gmail.com>
2024-04-12 12:00:01 +0200

    Update tests

--- tests/test_init_1.py
+++ tests/test_init_1.py
@@ -4,3 +4,10 @@ import pytest
 def test_init_1():
     import ELATE
 
+def test_init_2():
+    import ELATE
+    from ELATE import refdata
+
+    x = ELATE.Elastic(refdata.examples_3D['FAU'])
+    assert isinstance(x, ELATE.Elastic)
+
